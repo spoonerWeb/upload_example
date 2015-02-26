d709c27c4ab9936a2c5be5aad9d2f09451d1174e
Thomas Löffler <loeffler@spooner-web.de>
2015-02-26 13:43:44 +0100

    Update ext_emconf.php
    Tested in current 7.1.0 and it works.

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.4-6.3.99',
+			'typo3' => '6.2.4-7.1.99',
 		),
 		'conflicts' => array(
 		),
